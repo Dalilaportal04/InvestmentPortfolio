8bc100b51cf0bbda2f7224fd259824da66d560d1
emmabaumgartner <emmabaumgartner@ufl.edu>
2024-11-19 12:56:46 -0500

    test

--- Push_Test.cpp
+++ Push_Test.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Push_Test.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::ifstream file("data.csv");
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    return 0;
+}
